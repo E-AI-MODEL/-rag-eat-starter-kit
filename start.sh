4c342ab2975c0b5e6ed0613443d22f7b6227fc05
EAI <vis@emmauscollege.nl>
2026-06-26 00:57:35 +0200

    Install web extra from start script

--- start.sh
+++ start.sh
@@ -12,8 +12,7 @@ python3 -m venv .venv
 . .venv/bin/activate
 
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 -m pip install streamlit
+python3 -m pip install -e ".[web]"
 
 mkdir -p knowledge
 
